4500fcbc7393a2f1ba4adbe5f6cd613fc35bdef1
Maxim <korobprog@gmail.com>
2020-04-04 01:14:48 +1000

    3_save_dialog
    Signed-off-by: Maxim <korobprog@gmail.com>

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,9 +3,35 @@ import s from './Dialogs.module.css' ;
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andray
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Victor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hai</div>
+                <div className={s.dialog}>How are you?</div>
+                <div className={s.dialog}>Very well</div>
+
+            </div>
         </div>
+
 
     )
 }
