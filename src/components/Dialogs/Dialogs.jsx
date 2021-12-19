7f287ba813b94f4acea924f2583decd0b3913b6e
NOUTMAX\Max <korobprog@gmail.com>
2021-12-19 17:11:00 +1000

    Redux-Form ? login and Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,44 +1,59 @@
-import React from 'react';
-import s from './Dialogs.module.css';
+import React from "react";
+import s from "./Dialogs.module.css";
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Messange/Messange";
 import Button from "@material-ui/core/Button";
-import SendIcon from '@material-ui/icons/Send';
-import { Redirect } from 'react-router';
+import SendIcon from "@material-ui/icons/Send";
+import { Redirect } from "react-router";
+import { Field,  reduxForm, Form } from "redux-form";
 
 const Dialogs = (props) => {
-    let state = props.dialogsPage;
-    let dialogsElements = state.dialogs.map(dialog => <DialogItem name={dialog.name} key={dialog.id} avatar={dialog.avatar} />);
-    let messagesElements = state.messages.map(message => <Message message={message.message} key={message.id} />);
-    let newMessageBody = state.newMessageBody;
+  let state = props.dialogsPage;
+  let dialogsElements = state.dialogs.map((dialog) => (
+    <DialogItem name={dialog.name} key={dialog.id} avatar={dialog.avatar} />
+  ));
+  let messagesElements = state.messages.map((message) => (
+    <Message message={message.message} key={message.id} />
+  ));
+  let newMessageBody = state.newMessageBody;
 
-    let onSendMessageClick = () => {
-        props.sendMessage();
+  let onSendMessageClick = () => {
+    props.sendMessage();
+  };
+  let onNewSendMessageChange = (e) => {
+    let body = e.target.value;
+    props.updateNewMessageBody(body);
+  };
+  let addNewMessage = (values) => { //messages - newMessageBody
+    alert(values.newMessageBody)
+  };
+  if (!props.isAuth) return <Redirect to={"/Login"} />;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>{dialogsElements}</div>
+      <div className={s.messages}>
+        <div>{messagesElements}</div>
+        <AddMessageFormRedux onSubmit={addNewMessage} />
+      </div>
+    </div>
+  );
+};
 
-    }
-    let onNewSendMessageChange = (e) => {
-        let body = e.target.value;
-        props.updateNewMessageBody(body);
+const AddMessageForm = (props) => {
+  return (
+    <Form onSubmit={props.handleSubmit}>
+      <div>
+      <Field component="textarea" name="newMessageBody" placeholder="Enter your message" />
+      </div>
+      <div>
+        <Button 
+        color="secondary" size="small"endIcon={<SendIcon />} variant="contained">Send
+        </Button>
+      </div>
+    </Form>
+  );
+};
 
-    }
-if (!props.isAuth) return <Redirect to={"/Login"} /> ;
-    return (
-        <div className={s.dialogs}>
-            <div className={s.dialogsItems}>
-                {dialogsElements}
-            </div>
-            <div className={s.messages}>
-                <div>{messagesElements}</div>
-                <div>
-                    <div><textarea value={newMessageBody}
-                        onChange={onNewSendMessageChange}
-                        placeholder='Enter your message'></textarea></div>
-                    <div><Button color="secondary" size="small" endIcon={<SendIcon />} variant="contained" onClick={onSendMessageClick}>Send</Button></div>
-                </div>
-            </div>
-
-        </div>
-    )
-}
+const AddMessageFormRedux = reduxForm({form: "dialogAddMessageForm"})(AddMessageForm) ;
 
 export default Dialogs;
